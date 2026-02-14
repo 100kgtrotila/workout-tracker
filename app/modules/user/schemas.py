bfe3060d6c7b332f5f3b6a7ada2594023a553747
100kgtrotila <hirodevi192@gmail.com>
2026-02-14 15:14:40 +0200

    Add Pydantic schemas for `User` module
    - Introduced `UserBase`, `UserCreate`, `UserUpdate`, and `UserResponse` schemas in `schemas.py` for user data validation and serialization.

---
+++ app/modules/user/schemas.py
@@ -0,0 +1,22 @@
+from dataclasses import Field
+from datetime import datetime
+from typing import Optional
+
+from pydantic import PositiveInt,EmailStr
+
+from app.core.schemas import CustomModel
+
+class UserBase(CustomModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str = Field(min_lenght=6, max_lenght=128, description="User password")
+
+class UserUpdate(UserBase):
+    email: Optional[EmailStr]
+    password: Optional[str]
+
+class UserResponse(CustomModel):
+    id: PositiveInt
+    created_at: datetime
+
