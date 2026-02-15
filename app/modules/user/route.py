7772f6a81a46121f4b65373715d76f74a5cb49d9
100kgtrotila <hirodevi192@gmail.com>
2026-02-15 23:57:14 +0200

    Add user authentication routes for registration and login
    - Introduced `/register` route to handle user registration.
    - Added `/login` route for user authentication and token generation using JWT.

---
+++ app/modules/user/route.py
@@ -0,0 +1,32 @@
+from os import access
+
+from fastapi import APIRouter, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.annotation import Annotated
+
+from app.core.security import create_access_token
+from app.modules.user import service
+from app.core.db import get_db
+from app.modules.user.exceptions import UnauthorizedError
+from app.modules.user.schemas import UserResponse, UserCreate, Token
+
+router = APIRouter(prefix="/user", tags=["User"])
+
+@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+async def register_user(user_data: UserCreate, session: AsyncSession = Depends(get_db)):
+   return await service.create_user(session, user_data)
+
+@router.post("/login", response_model=UserResponse)
+async def login_user(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        session: AsyncSession = Depends(get_db)):
+
+    user = await service.authenticate_user(session, form_data.email, form_data.password)
+
+    if not user:
+        raise UnauthorizedError("Invalid email or password")
+
+    access_token = create_access_token(data={"sub": user.email})
+
+    return Token(access_token=access_token, token_type="bearer")
