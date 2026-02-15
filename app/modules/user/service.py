3b8e44630c208ce79b90a208ab49d42bed569b33
100kgtrotila <hirodevi192@gmail.com>
2026-02-15 23:24:45 +0200

    Add `create_user` function to user service for registration and enhance `authenticate_user` logic
    - Implemented `create_user` to handle user registration, including validation, password hashing, and database insertion.
    - Updated `authenticate_user` to return `None` instead of `False` for invalid credentials.

--- app/modules/user/service.py
+++ app/modules/user/service.py
@@ -1,23 +1,44 @@
+from typing import Optional
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.security import verify_password, DUMMY_HASH
+from app.core.exceptions import BadRequestError
+from app.core.security import verify_password, DUMMY_HASH, get_password_hash
 from app.modules.user.models import User
+from app.modules.user.schemas import UserCreate
 
 
-async def authenticate_user(session: AsyncSession, username: str, password: str) -> User | bool:
-    query =select(User).where(User.email==username)
+async def authenticate_user(
+    session: AsyncSession, email: str, password: str
+) -> Optional[User]:
+    query = select(User).where(User.email == email)
     result = await session.execute(query)
-
     db_user = result.scalar_one_or_none()
 
     if not db_user:
         verify_password(password, DUMMY_HASH)
-        return False
+        return None
 
-    if not verify_password(password, hashed_password=db_user.password_hash):
-        return False
+    if not verify_password(password, db_user.password_hash):
+        return None
 
     return db_user
 
 
+async def create_user(session: AsyncSession, user_data: UserCreate) -> User:
+    query = select(User).where(User.email == user_data.email)
+    result = await session.execute(query)
+    existing_user = result.scalar_one_or_none()
+
+    if existing_user:
+        raise BadRequestError("This email address is already registered.")
+
+    hashed_password = get_password_hash(user_data.password)
+
+    new_user = User(email=user_data.email, password_hash=hashed_password)
+
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+
+    return new_user
