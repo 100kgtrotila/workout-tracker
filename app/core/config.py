2cda7e840d797c9c40c2c6a8f1dfcec8eb3f4cd0
100kgtrotila <hirodevi192@gmail.com>
2026-02-17 21:27:03 +0200

    Add authentication config variables to `config.py`

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,10 @@ class Settings(BaseSettings):
     POSTGRES_HOST: str = "db"
     POSTGRES_PORT: int = 5432
 
+    SECRET_KEY: str
+    ALGORITHM:str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
     @property
     def DATABASE_URL(self) -> str:
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
