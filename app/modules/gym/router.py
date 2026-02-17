8f7d85562c38794655c775a813acc669a09fb21e
100kgtrotila <hirodevi192@gmail.com>
2026-02-18 00:56:16 +0200

    Restrict exercise routes to admin-only access using `admin_only` dependency

--- app/modules/gym/router.py
+++ app/modules/gym/router.py
@@ -3,10 +3,13 @@ from typing import List
 from fastapi import APIRouter, status
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.security import admin_only
 from app.modules.gym import service
 
 from app.core.db import get_db
 from app.modules.gym.schemas import ExerciseResponse, ExerciseCreate, ExerciseUpdate
+from app.modules.user.models import User
 
 router = APIRouter(prefix="/exercise", tags=["Exercise"])
 
@@ -19,14 +22,17 @@ async def get_exercise_by_id(exercise_id: int, session: AsyncSession = Depends(g
     return await service.get_exercise_by_id(session, exercise_id)
 
 @router.post("/", response_model=ExerciseResponse, status_code=status.HTTP_201_CREATED)
-async def create_exercise(data: ExerciseCreate, session: AsyncSession = Depends(get_db)):
+async def create_exercise(data: ExerciseCreate, session: AsyncSession = Depends(get_db),
+                          admin_user: User = Depends(admin_only)):
     return await service.create_exercise(session, data)
 
 @router.patch("/{exercise_id}", response_model=ExerciseResponse, status_code=status.HTTP_200_OK)
-async def update_exercise(data: ExerciseUpdate, exercise_id: int, session: AsyncSession = Depends(get_db)):
+async def update_exercise(data: ExerciseUpdate, exercise_id: int, session: AsyncSession = Depends(get_db),
+                          admin_user: User = Depends(admin_only)):
     return await service.update_exercise(session, exercise_id, data)
 
 @router.delete("/{exercise_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_exercise(exercise_id: int, session: AsyncSession = Depends(get_db)):
+async def delete_exercise(exercise_id: int, session: AsyncSession = Depends(get_db),
+                          admin_user: User = Depends(admin_only)):
     return await service.del_exercise(session, exercise_id)
 
