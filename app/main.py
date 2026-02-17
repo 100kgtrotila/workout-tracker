30005b3ae17b8a2ccfdd731317a60ace5e5747ff
100kgtrotila <hirodevi192@gmail.com>
2026-02-17 20:52:29 +0200

    Register routers for `gym`, `user`, and `training` modules in FastAPI app.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.modules.gym.router import router as gym_router
+from app.modules.user.router import router as user_router
+from app.modules.training.router import router as training_router
+
 app = FastAPI(title="Workout Tracker")
+app.include_router(gym_router)
+app.include_router(user_router)
+app.include_router(training_router)
+
+
 
 
 if __name__ == "__main__":
