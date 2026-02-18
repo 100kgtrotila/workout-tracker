25effa182e22b5cb794b5e8719c7a643e3b70ac9
100kgtrotila <hirodevi192@gmail.com>
2026-02-18 15:34:33 +0200

    Refactor `Settings` class: replace PostgreSQL connection fields with `DATABASE_URL`, add `REDIS_URL`, and enhance configuration with computed fields and stricter validation

--- app/core/config.py
+++ app/core/config.py
@@ -1,20 +1,23 @@
+from pydantic import RedisDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    POSTGRES_HOST: str = "db"
-    POSTGRES_PORT: int = 5432
-
+    DATABASE_URL: str
+    REDIS_URL: RedisDsn
     SECRET_KEY: str
-    ALGORITHM:str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
+    @computed_field
     @property
-    def DATABASE_URL(self) -> str:
-        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+    def ASYNC_DATABASE_URL(self) -> str:
+        return self.DATABASE_URL.replace(
+            "postgresql://", "postgresql+asyncpg://"
+        )
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(
+        env_file=".env", case_sensitive=True, extra="ignore"
+    )
 
 settings = Settings()
